fcc812784af58e89c49c02685f69c6fa7771a470
조하은 <jhe93@naver.com>
2026-05-17 06:19:03 +0900

    fix: Swiper 관련 에러 해결

--- src/components/artist/search-result-section.tsx
+++ src/components/artist/search-result-section.tsx
@@ -1,8 +1,9 @@
 ﻿import SearchResultItem from './search-result-item'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import XIcon from '@/assets/icons/common/x-icon.svg?react'
-import { Pagination } from 'swiper/modules'
+import type { Swiper as SwiperType } from 'swiper'
 import 'swiper/css'
+import { useState } from 'react'
 
 interface SearchResultSectionProps {
   query: string
@@ -10,6 +11,8 @@ interface SearchResultSectionProps {
 
 export function SearchResultSection({ query }: SearchResultSectionProps) {
   // TODO: useSearchArtistsQuery(query) 로 교체
+  const [activeIndex, setActiveIndex] = useState(0)
+
   const length = 7
 
   const mockData = Array.from({ length }, (_, index) => ({
@@ -25,8 +28,15 @@ export function SearchResultSection({ query }: SearchResultSectionProps) {
   return (
     <>
       {mockData.length > 0 ? (
-        <div className="flex flex-col gap-5">
-          <Swiper slidesPerView={1}>
+        <div className="flex flex-col gap-5 overflow-hidden">
+          <Swiper
+            slidesPerView={1}
+            className="w-full"
+            pagination={{ clickable: true }}
+            onSlideChange={(swiper: SwiperType) =>
+              setActiveIndex(swiper.activeIndex)
+            }
+          >
             {chunkedData.map((slideItems, slideIndex) => (
               <SwiperSlide key={slideIndex}>
                 <div className="flex flex-col gap-2">
@@ -37,7 +47,18 @@ export function SearchResultSection({ query }: SearchResultSectionProps) {
               </SwiperSlide>
             ))}
           </Swiper>
-          <div className="flex w-full items-center justify-center">...</div>
+          <div className="flex w-full items-center justify-center gap-1.5">
+            {chunkedData.map((_, i) => (
+              <div
+                key={i}
+                className={`rounded-full transition-all duration-300 ${
+                  i === activeIndex
+                    ? 'bg-mono-gray-7 size-3' // 활성 — 길쭉한 pill
+                    : 'bg-mono-gray-3 size-3' // 비활성 — 작은 원
+                }`}
+              />
+            ))}
+          </div>
         </div>
       ) : (
         <div className="flex flex-1 flex-col items-center justify-center">
