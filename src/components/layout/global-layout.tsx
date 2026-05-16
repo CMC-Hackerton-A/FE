ab23bb22402c966398f537eef78c67f9078866dc
조하은 <jhe93@naver.com>
2026-05-17 02:19:05 +0900

    fix: global-layout수정

--- src/components/layout/global-layout.tsx
+++ src/components/layout/global-layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router'
 
 export default function GlobalLayout() {
+  //
   return (
     <div className="bg-mono-gray-1 flex min-h-screen justify-center">
       <div className="relative min-h-screen w-full max-w-[430px] overflow-x-hidden bg-white">
