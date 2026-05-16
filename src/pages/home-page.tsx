f4f26958a061a60feb2295eac431aa6407f148ad
조하은 <jhe93@naver.com>
2026-05-17 04:15:51 +0900

    fix: HomePage 로직 수정

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,8 +1,14 @@
-import TopArtistSection from '@/components/artist/top-artist-section'
-import HeroSection from '@/components/main/hero-section'
+import { useState } from 'react'
+import { HeroSection } from '@/components/main/hero-section'
+import { HomeContentPanel } from '@/components/main/HomeContentPanel'
+import { TopArtistSection } from '@/components/artist/top-artist-section'
+import { SearchResultSection } from '@/components/artist/search-result-section'
 import Header from '@/components/ui/header'
 
 export default function HomePage() {
+  // 검색어
+  const [searchQuery, setSearchQuery] = useState('')
+
   return (
     <main className="relative h-dvh w-full overflow-hidden bg-white">
       <div className="fixed inset-x-0 top-6 z-50 flex justify-center">
@@ -10,10 +16,19 @@ export default function HomePage() {
           <Header />
         </div>
       </div>
-      <HeroSection />
+
+      <HeroSection onSearch={setSearchQuery} searchedText={searchQuery} />
 
       <div className="relative z-10 -mt-6 h-[calc(60dvh+24px)]">
-        <TopArtistSection />
+        <HomeContentPanel
+          title={searchQuery ? '검색 결과' : '지금 가장 빛나는 별 TOP 10'}
+        >
+          {searchQuery ? (
+            <SearchResultSection query={searchQuery} />
+          ) : (
+            <TopArtistSection />
+          )}
+        </HomeContentPanel>
       </div>
     </main>
   )
